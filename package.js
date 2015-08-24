1bd6aab35eb58131004c734cec47d73f07fa0c20
bengfarrell <ben@benfarrell.com>
2015-08-23 17:48:43 -0700

    added compression

--- package.js
+++ package.js
@@ -1,5 +1,7 @@
 var Config = require('./src/utils/Config');
 var Logging = require('./src/utils/Logging');
+var zipdir = require('zip-dir');
+var path = require('path');
 
 var cfg = new Config().load('config.json');
 cfg.log = Logging.console;
@@ -15,5 +17,12 @@ var duration = parseInt(process.argv[3]) || 5400;
 showbuilder.on(BuildShow.prototype.COMPLETE, function(stats) {
     cfg.log("SharkAttack", "Show " + show + " created" );
     Logging.recordTaskRun( { start: starttime, end: new Date(), name: 'packaging', details: 'Show ' + show + ' created' } );
+
+    if (cfg.packaging.compress === true) {
+        zipdir( cfg.packaging.showLocation + path.sep + show, { saveTo: cfg.packaging.showLocation + path.sep + show + '.zip' }, function (err, buffer) {
+            cfg.log("SharkAttack", "Show " + show + " zipped" );
+            Logging.recordTaskRun( { start: starttime, end: new Date(), name: 'packaging', details: 'Show ' + show + ' zipped' } );
+        });
+    }
 });
 showbuilder.run(show, duration, cfg.libLocation);
