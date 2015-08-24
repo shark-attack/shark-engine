7cafe7a5312661e19cafc9bcc4ee82bfb4612ddf
bengfarrell <ben@benfarrell.com>
2015-08-23 23:06:11 -0700

    require syntax error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var engine = {
     config: require('./config'),
-    package: reqiure('./package'),
+    package: require('./package'),
     clean: require('./clean'),
     discover: require('./discover'),
     schedule: require('./schedule')
