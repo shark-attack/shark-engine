f239ae25c5a3d015efece9fb0b6672f0d22bfd51
Ben Farrell <ben@benfarrell.com>
2015-08-24 21:55:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+var File = require('./src/utils/File');
 var Config = require('./src/utils/Config');
 var cfg = new Config().load( __dirname + '/config.json');
+
+File.prototype.ensureDirectoriesExist([cfg.applicationStorage, cfg.mediaDirectory]);
 module.exports = cfg;
